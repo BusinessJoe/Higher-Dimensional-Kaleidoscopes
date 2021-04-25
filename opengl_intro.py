d8eec56e1d78265deaa396e4d4518bc50638861c
Lukas Boelling <>
2021-04-25 14:29:51 -0400

    Create simple openGL demo

--- opengl_intro.py
+++ opengl_intro.py
@@ -6,6 +6,9 @@ from PyQt5 import QtOpenGL
 import OpenGL.GL as gl
 from OpenGL import GLU
 
+from OpenGL.arrays import vbo       # vertex buffer objects
+import numpy as np
+
 import sys
 
 
@@ -15,6 +18,138 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.resize(300, 300)
         self.setWindowTitle("Hello OpenGL App")
+
+        self.glWidget = GLWidget(self)
+        self.initGUI()
+
+        timer = QtCore.QTimer(self)
+        timer.setInterval(20)
+        timer.timeout.connect(self.glWidget.updateGL)
+        timer.start()
+
+    def initGUI(self):
+        central_widget = QtWidgets.QWidget()
+        gui_layout = QtWidgets.QVBoxLayout()
+        central_widget.setLayout(gui_layout)
+
+        self.setCentralWidget(central_widget)
+
+        gui_layout.addWidget(self.glWidget)
+
+        sliderX = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        sliderX.valueChanged.connect(lambda val: self.glWidget.setRotX(val))
+        sliderX.setMinimum(0)
+        sliderX.setMaximum(360)
+
+        sliderY = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        sliderY.valueChanged.connect(lambda val: self.glWidget.setRotY(val))
+        sliderY.setMinimum(0)
+        sliderY.setMaximum(360)
+
+        sliderZ = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        sliderZ.valueChanged.connect(lambda val: self.glWidget.setRotZ(val))
+        sliderZ.setMinimum(0)
+        sliderZ.setMaximum(360)
+
+        gui_layout.addWidget(sliderX)
+        gui_layout.addWidget(sliderY)
+        gui_layout.addWidget(sliderZ)
+
+
+class GLWidget(QtOpenGL.QGLWidget):
+    def __init__(self, parent=None):
+        QtOpenGL.QGLWidget.__init__(self, parent)
+        self.parent = parent
+
+    def initializeGL(self) -> None:
+        self.qglClearColor(QtGui.QColor(0, 0, 255))     # initialize screen to blue
+        gl.glEnable(gl.GL_DEPTH_TEST)                   # enable depth testing
+
+        self.init_geometry()
+
+        self.rotX = 0.0
+        self.rotY = 0.0
+        self.rotZ = 0.0
+
+    def resizeGL(self, w: int, h: int) -> None:
+        gl.glViewport(0, 0, w, h)
+        gl.glMatrixMode(gl.GL_PROJECTION)
+        gl.glLoadIdentity()
+
+        aspect = w / h
+        GLU.gluPerspective(45, aspect, 1, 100)
+        gl.glMatrixMode(gl.GL_MODELVIEW)
+
+    def paintGL(self):
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+
+        gl.glPushMatrix()       # push the current matrix to the current stack
+
+        gl.glTranslate(0.0, 0.0, -50.0)     # third, translate cube to specified depth
+        gl.glScale(20.0, 20.0, 20.0)        # second, scale cube
+        gl.glRotate(self.rotX, 1, 0, 0)
+        gl.glRotate(self.rotY, 0, 1, 0)
+        gl.glRotate(self.rotZ, 0, 0, 1)
+        gl.glTranslate(-0.5, -0.5, -0.5)    # first, translate cube center to origin
+
+        gl.glEnableClientState(gl.GL_VERTEX_ARRAY)
+        gl.glEnableClientState(gl.GL_COLOR_ARRAY)
+
+        gl.glVertexPointer(3, gl.GL_FLOAT, 0, self.vert_VBO)
+        gl.glColorPointer(3, gl.GL_FLOAT, 0, self.color_VBO)
+
+        gl.glDrawElements(gl.GL_QUADS, len(self.cube_idx_array), gl.GL_UNSIGNED_INT, self.cube_idx_array)
+
+        gl.glDisableClientState(gl.GL_VERTEX_ARRAY)
+        gl.glDisableClientState(gl.GL_COLOR_ARRAY)
+
+        gl.glPopMatrix()    # restore the previous modelview matrix
+
+    def init_geometry(self):
+        self.cube_vtx_array = np.array(
+            [[0.0, 0.0, 0.0],
+             [1.0, 0.0, 0.0],
+             [1.0, 1.0, 0.0],
+             [0.0, 1.0, 0.0],
+             [0.0, 0.0, 1.0],
+             [1.0, 0.0, 1.0],
+             [1.0, 1.0, 1.0],
+             [0.0, 1.0, 1.0]]
+        )
+        self.vert_VBO = vbo.VBO(np.reshape(self.cube_vtx_array,
+                                           (1, -1)).astype(np.float32))
+        self.vert_VBO.bind()
+
+        self.cube_clr_array = np.array(
+            [[0.0, 0.0, 0.0],
+             [1.0, 0.0, 0.0],
+             [1.0, 1.0, 0.0],
+             [0.0, 1.0, 0.0],
+             [0.0, 0.0, 1.0],
+             [1.0, 0.0, 1.0],
+             [1.0, 1.0, 1.0],
+             [0.0, 1.0, 1.0]]
+        )
+        self.color_VBO = vbo.VBO(np.reshape(self.cube_clr_array, (1, -1)).astype(np.float32))
+        self.color_VBO.bind()
+
+        self.cube_idx_array = np.array(
+            [0, 1, 2, 3,
+             3, 2, 6, 7,
+             1, 0, 4, 5,
+             2, 1, 5, 6,
+             0, 3, 7, 4,
+             7, 6, 5, 4]
+        )
+
+    def setRotX(self, val):
+        self.rotX = val
+
+    def setRotY(self, val):
+        self.rotY = val
+
+    def setRotZ(self, val):
+        self.rotZ = val
 
 
 if __name__ == "__main__":
