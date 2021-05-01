2708f2a1a4ce53e2cca9ff41e50896cd1c155681
Lukas Boelling <lukasboelling002@gmail.com>
2021-05-01 13:27:57 -0400

    Add cube

--- opengl_intro/cube.py
+++ opengl_intro/cube.py
@@ -47,10 +47,47 @@ class GLWidget(QtOpenGL.QGLWidget):
 
         # positions followed by rgb values
         vertices = np.array([
-            0.5, 0.5, 0.0,      1.0, 0.0, 0.0,
-            0.5, -0.5, 0.0,     1.0, 1.0, 0.0,
-            -0.5, -0.5, 0.0,    0.0, 0.0, 1.0,
-            -0.5, 0.5, 0.0,     1.0, 0.0, 1.0
+            -0.5, -0.5, -0.5, 0.0, 0.0,
+            0.5, -0.5, -0.5, 1.0, 0.0,
+            0.5, 0.5, -0.5, 1.0, 1.0,
+            0.5, 0.5, -0.5, 1.0, 1.0,
+            -0.5, 0.5, -0.5, 0.0, 1.0,
+            -0.5, -0.5, -0.5, 0.0, 0.0,
+
+            -0.5, -0.5, 0.5, 0.0, 0.0,
+            0.5, -0.5, 0.5, 1.0, 0.0,
+            0.5, 0.5, 0.5, 1.0, 1.0,
+            0.5, 0.5, 0.5, 1.0, 1.0,
+            -0.5, 0.5, 0.5, 0.0, 1.0,
+            -0.5, -0.5, 0.5, 0.0, 0.0,
+
+            -0.5, 0.5, 0.5, 1.0, 0.0,
+            -0.5, 0.5, -0.5, 1.0, 1.0,
+            -0.5, -0.5, -0.5, 0.0, 1.0,
+            -0.5, -0.5, -0.5, 0.0, 1.0,
+            -0.5, -0.5, 0.5, 0.0, 0.0,
+            -0.5, 0.5, 0.5, 1.0, 0.0,
+
+            0.5, 0.5, 0.5, 1.0, 0.0,
+            0.5, 0.5, -0.5, 1.0, 1.0,
+            0.5, -0.5, -0.5, 0.0, 1.0,
+            0.5, -0.5, -0.5, 0.0, 1.0,
+            0.5, -0.5, 0.5, 0.0, 0.0,
+            0.5, 0.5, 0.5, 1.0, 0.0,
+
+            -0.5, -0.5, -0.5, 0.0, 1.0,
+            0.5, -0.5, -0.5, 1.0, 1.0,
+            0.5, -0.5, 0.5, 1.0, 0.0,
+            0.5, -0.5, 0.5, 1.0, 0.0,
+            -0.5, -0.5, 0.5, 0.0, 0.0,
+            -0.5, -0.5, -0.5, 0.0, 1.0,
+
+            -0.5, 0.5, -0.5, 0.0, 1.0,
+            0.5, 0.5, -0.5, 1.0, 1.0,
+            0.5, 0.5, 0.5, 1.0, 0.0,
+            0.5, 0.5, 0.5, 1.0, 0.0,
+            -0.5, 0.5, 0.5, 0.0, 0.0,
+            -0.5, 0.5, -0.5, 0.0, 1.0,
         ], np.float32)
         indices = np.array([
             0, 1, 3,
@@ -59,7 +96,7 @@ class GLWidget(QtOpenGL.QGLWidget):
 
         self.vertex_array = gl.glGenVertexArrays(1)
         self.vertex_buffer = gl.glGenBuffers(1)
-        self.element_buffer = gl.glGenBuffers(1)
+        # self.element_buffer = gl.glGenBuffers(1)
 
         # 1. bind Vertex Array Object
         gl.glBindVertexArray(self.vertex_array)
@@ -67,18 +104,18 @@ class GLWidget(QtOpenGL.QGLWidget):
         gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.vertex_buffer)
         gl.glBufferData(gl.GL_ARRAY_BUFFER, len(vertices) * 4, vertices, gl.GL_STATIC_DRAW)
         # 3. copy our index array in a element buffer for OpenGL to use
-        gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, self.element_buffer)
-        gl.glBufferData(gl.GL_ELEMENT_ARRAY_BUFFER, len(indices) * 4, indices, gl.GL_STATIC_DRAW)
+        # gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, self.element_buffer)
+        # gl.glBufferData(gl.GL_ELEMENT_ARRAY_BUFFER, len(indices) * 4, indices, gl.GL_STATIC_DRAW)
         # 4. then set our vertex attributes pointers
         # position attribute
-        gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 6 * 4, ctypes.c_void_p(0))
+        gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 5 * 4, ctypes.c_void_p(0))
         gl.glEnableVertexAttribArray(0)
         # color attribute
-        gl.glVertexAttribPointer(1, 3, gl.GL_FLOAT, gl.GL_FALSE, 6 * 4, ctypes.c_void_p(3 * 4))
-        gl.glEnableVertexAttribArray(1)
+        # gl.glVertexAttribPointer(1, 3, gl.GL_FLOAT, gl.GL_FALSE, 6 * 4, ctypes.c_void_p(3 * 4))
+        # gl.glEnableVertexAttribArray(1)
 
-        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0)
-        gl.glBindVertexArray(0)
+        # gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0)
+        # gl.glBindVertexArray(0)
 
         # gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_LINE)
 
@@ -106,8 +143,9 @@ class GLWidget(QtOpenGL.QGLWidget):
 
         # render
         gl.glBindVertexArray(self.vertex_array)
-        gl.glDrawElements(gl.GL_TRIANGLES, 6, gl.GL_UNSIGNED_INT, ctypes.c_void_p(0))
-        gl.glBindVertexArray(0)
+        # gl.glDrawElements(gl.GL_TRIANGLES, 6, gl.GL_UNSIGNED_INT, ctypes.c_void_p(0))
+        gl.glDrawArrays(gl.GL_TRIANGLES, 0, 36)
+        # gl.glBindVertexArray(0)
 
     def resizeGL(self, w: int, h: int) -> None:
         gl.glViewport(0, 0, w, h)
--- opengl_intro/shader.vert
+++ opengl_intro/shader.vert
@@ -1,6 +1,5 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aColor;
 
 out vec3 ourColor;
 
@@ -11,5 +10,5 @@ uniform mat4 projection;
 void main()
 {
     gl_Position = projection * view * model * vec4(aPos, 1.0);
-    ourColor = aColor;
+    ourColor = vec3(1.0, 0.0, 0.0);
 }
